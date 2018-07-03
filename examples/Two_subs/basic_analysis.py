3e9612624a11d24a424d1fed3ba6b799626f45aa
Xuefei Cao <xcstf01@gmail.com>
2018-07-03 17:00:08 -0400

    fix eva

--- examples/Two_subs/basic_analysis.py
+++ examples/Two_subs/basic_analysis.py
@@ -12,7 +12,7 @@ from scdn.evaluation import eva
 folder_name = ['Analysis/1/', 'Analysis/2/']
 data_file = ['data/fMRI_1.txt', 'data/fMRI_2.txt']
 real_parameter_file = 'data/real.pkl' # dict for real para
-stimuli_folder = ['data/stimuli/']*2 # This folder only contains evt files
+stimuli_folder = ['data/stimuli/']*2 # This folder only contains evt files, two subjects share same stimuli 
 dt = 0.72 #TR of fMRI 
 scdn_multi_sub(folder_name, data_file, stimuli_folder, val_pair=(0,1), dt=dt, lam=[0.1,0.01,1,10,50,100], mu=[1], mu_1=[1], mu_2=[1], lam_1=[0], tol=1e-2, max_iter=100, N=50, fold=0.5, share_stimuli=True, num_cores=1)
 eva(folder_name, real_parameters=real_parameter_file, saved_folder_name='')
